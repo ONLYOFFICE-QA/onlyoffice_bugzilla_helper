51bde098540caf8a4a3e2b56f698089acfc767e2
Pavel Lobashov <shockwavenn@gmail.com>
2020-04-24 16:10:22 +0300

    Decrease metric and split two file

--- spec/onlyoffice_bugzilla_helper/bugzilla_helper_spec.rb
+++ spec/onlyoffice_bugzilla_helper/bugzilla_helper_spec.rb
@@ -12,32 +12,4 @@ RSpec.describe OnlyofficeBugzillaHelper::BugzillaHelper do
   it 'check bugzilla url is not nil' do
     expect(bugzilla.url).not_to be_nil
   end
-
-  it 'bug_id_from_string correct url' do
-    string = "#{bugzilla.url}/show_bug.cgi?id=32296"
-    expect(bugzilla.bug_id_from_string(string)).to eq(32_296)
-  end
-
-  it 'bug_id_from_string that looks like correct bugzilla bug url' do
-    string = "#{bugzilla.url}/test?id=32296"
-    expect(bugzilla.bug_id_from_string(string)).to be_nil
-  end
-
-  it 'bug_id_from_string that looks like correct bugzilla bug param' do
-    string = "#{bugzilla.url}/show_bug.cgi?test=32296"
-    expect(bugzilla.bug_id_from_string(string)).to be_nil
-  end
-
-  it 'bug_id_from_string without digits' do
-    expect(bugzilla.bug_id_from_string('test')).to be_nil
-  end
-
-  it 'bug_id_from_string with spaces' do
-    expect(bugzilla.bug_id_from_string('test test')).to be_nil
-  end
-
-  it 'bug_id_from_string with digits but another url' do
-    string = 'http://bugzserver/show_bug.cgi?id=31427'
-    expect(bugzilla.bug_id_from_string(string)).to be_nil
-  end
 end
