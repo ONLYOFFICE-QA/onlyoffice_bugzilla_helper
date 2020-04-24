63871aa47a845c30ee667d701a7634217122fd28
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-04-24 16:18:37 +0300

    Rubocop 0.82.0 updates (#22)
    * Actualize RuboCop todo to 0.82.0
    
    * Add support of rubocop-performance
    
    * Decrease metrics
    
    * Fix RuboCop `Performance/RegexpMatch` cop
    
    * Decrease metric and split two file

--- spec/onlyoffice_bugzilla_helper/bugzilla_helper_update_bug_spec.rb
+++ spec/onlyoffice_bugzilla_helper/bugzilla_helper_update_bug_spec.rb
@@ -4,21 +4,33 @@ require 'spec_helper'
 
 RSpec.describe OnlyofficeBugzillaHelper::BugzillaHelper, '#update_bug' do
   let(:bugzilla) { described_class.new }
+  let(:data) { bugzilla.bug_data(BUG_ID_TEST_STATUS) }
 
   before do
     bugzilla.update_bug(BUG_ID_TEST_STATUS, status: 'NEW')
   end
 
-  it 'Change status to RESOLVED FIXED' do
-    data = bugzilla.bug_data(BUG_ID_TEST_STATUS)
-    expect(data['status']).not_to eq('RESOLVED')
-    expect(data['resolution']).not_to eq('FIXED')
+  it 'By default bug status is NEW' do
+    expect(data['status']).to eq('NEW')
+  end
 
-    bugzilla.update_bug(BUG_ID_TEST_STATUS,
-                        status: 'RESOLVED',
-                        resolution: 'FIXED')
-    data = bugzilla.bug_data(BUG_ID_TEST_STATUS)
-    expect(data['status']).to eq('RESOLVED')
-    expect(data['resolution']).to eq('FIXED')
+  it 'By default bug resolution is empty' do
+    expect(data['resolution']).to eq('')
+  end
+
+  describe 'After change to RESOLVED FIXED' do
+    before do
+      bugzilla.update_bug(BUG_ID_TEST_STATUS,
+                          status: 'RESOLVED',
+                          resolution: 'FIXED')
+    end
+
+    it 'After change status is Resolved' do
+      expect(data['status']).to eq('RESOLVED')
+    end
+
+    it 'After change resolution is Fixed' do
+      expect(data['resolution']).to eq('FIXED')
+    end
   end
 end
